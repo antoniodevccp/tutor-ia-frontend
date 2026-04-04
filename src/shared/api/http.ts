57f037e57ee614137207c03cc5e1f13ec180fe72
Antonio-igo <107630272+Antonio-igo@users.noreply.github.com>
2026-04-04 14:45:30 -0300

    chore: initial frontend setup

---
+++ src/shared/api/http.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const http = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
