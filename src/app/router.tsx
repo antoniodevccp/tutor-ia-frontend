44c554891e456ff7f88c7af92f7178da275beeb5
Antonio-igo <107630272+Antonio-igo@users.noreply.github.com>
2026-04-25 12:11:04 -0400

    register entry point

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,11 +3,16 @@ import AppLayout from '../shared/layouts/AppLayout'
 import LoginPage from '../features/auth/LoginPage'
 import StartPage from '../features/catalog/StartPage'
 import ProtectedRoute from './ProtectedRoute'
+import RegisterPage from '@/features/auth/RegisterPage'
 
 export const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />,
+  },
+  {
+  path: '/register',
+  element: <RegisterPage />,
   },
   {
     path: '/',
